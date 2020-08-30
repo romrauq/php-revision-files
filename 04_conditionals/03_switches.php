d85625de689de60b30e1a78ec8a719168618ca01
John Quarmor <johnequarmor@gmail.com>
2020-08-30 06:52:14 +0000

    Added switches revision file.

---
+++ 04_conditionals/03_switches.php
@@ -0,0 +1,28 @@
+<?php
+		// Switches
+
+		$weekday = "Wednesday";
+
+		switch ($weekday) {
+			case "Monday":
+				echo "It's Monday, get ready to start your week with a healthy grind!";
+				break;
+			case "Tuesday":
+				echo "It's Tuseday, Keep at your grind!";
+				break;
+			case "Wednesday":
+				echo "Wednesday now, we're halfway through the week!";
+				break;
+			case "Thursday":
+				echo "Thursday, dont get lax now. keep up the good hustle!";
+				break;
+			case "Friday":
+				echo "Friday, one final work day before the weekend!";
+				break;
+				
+			default:
+				echo "Its the Weekend! Take some time to recuperate and relax.";
+				break;
+		}
+
+?>
