3ff7e2ae6d207ec815cd082e6fa0dc160301486b
John Quarmor <johnequarmor@gmail.com>
2020-08-30 20:12:47 +0000

    Added dates revision file

---
+++ 05_dates_&_time/01_dates.php
@@ -0,0 +1,25 @@
+<?php
+
+	echo date('d');		// Returns numerical current day of the month.
+		echo "<br>";
+	echo date('m');		// Returns numerial value of the current month of the year.
+		echo "<br>";
+	echo date('Y');		// Returns the full numerical value of the current year.
+		echo "<br>";
+	echo date('d');		// Returns the abbreviated numerical value of the current year.
+		echo "<br>";
+	echo date('l');		// Returns the string value of the day of the week.
+		echo "<br>";
+	echo date('d/m/y'); // Returns the current date in numerical value with abbreviated year.
+		echo "<br>";
+	echo date('d-m-y'); // Returns the current date in numerical value with abbreviated year.
+		echo "<br>";
+	echo date('d-m-Y'); // Returns the current date in numerical value with full year.
+		echo "<br>";
+
+	
+
+	
+
+
+?>
