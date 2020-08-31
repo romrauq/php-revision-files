4a14ba08d4f14af6e72679c094d6e9c537eb67b2
John Quarmor <johnequarmor@gmail.com>
2020-08-31 10:50:20 +0000

    Created simple webpage which uses 'require' statements in all pages.

---
+++ 06_include_&_require/02_using_require/pages/contact.php
@@ -0,0 +1,16 @@
+<?php require '../require/pages-head-tags.php'; ?>
+
+		<div class="navbar">
+			<ul>
+				<li><a href="../index.php">HomePage</a></li>
+				<li><a href="./products.php">Products</a></li>
+				<li><a href="./services.php">Services</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</div>
+
+		<div class="header">
+			<h1>Contact</h1>
+		</div>
+
+<?php require '../require/end-tags.php'; ?>
