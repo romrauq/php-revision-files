80339dc2d45d7fd55079ba448fd04f5dcd5f4690
John Quarmor <johnequarmor@gmail.com>
2020-09-02 07:20:00 +0000

    Renames revision file's name

---
+++ 09_common_string_functions/00_string_functions_list.php
@@ -0,0 +1,17 @@
+<?php 
+	
+	# STRING FUNCTIONS:
+	# substr() - returns a portion of a string.
+	# strlen() - returns the length of a string.
+	# strpos() - returns the first occurence of a specified sub string.
+	# strrpos() - returns the last occurence of a specified sub string.
+	# trim() - removes whitespace from a passed string value.
+	# strtoupper() - converts passed string text entirely to uppercase.
+	# strtolower() - converts passed string text entirely to lowercase.
+	# ucwords() - capitalizes the start of every word passed.
+	# str_replace() - replaces all occurences of a passed string with a replacement string.
+	# is_string() - tests an argument and returns boolean true if argument is a string and false if not a string.
+	# gzcompress() - compresses a string.
+	# gzuncompress() - uncompresses a compressed string.
+	
+?>
