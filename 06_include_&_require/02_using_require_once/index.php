2471916f6b0239bf55bc4da73e5afa97fe218255
John Quarmor <johnequarmor@gmail.com>
2020-09-02 09:30:45 +0000

    Added require_once revision folder & web files.

---
+++ 06_include_&_require/02_using_require_once/index.php
@@ -0,0 +1,15 @@
+<?php require_once './require/index-head-tags.php'; ?>
+
+		<div class="navbar">
+			<ul>
+				<li><a href="#">HomePage</a></li>
+				<li><a href="./pages/products.php">Products</a></li>
+				<li><a href="./pages/services.php">Services</a></li>
+				<li><a href="./pages/contact.php">Contact</a></li>
+			</ul>
+		</div>
+
+		<div class="header">
+			<h1>HomePage</h1>
+		</div>
+<?php require_once './require/end-tags.php'; ?>
---
+++ 06_include_&_require/02_using_require_once/pages/products.php
@@ -0,0 +1,17 @@
+<?php require_once '../require/pages-head-tags.php'; ?>
+
+<body>
+		<div class="navbar">
+			<ul>
+				<li><a href="../index.php">HomePage</a></li>
+				<li><a href="#">Products</a></li>
+				<li><a href="./services.php">Services</a></li>
+				<li><a href="./contact.php">Contact</a></li>
+			</ul>
+		</div>
+		
+		<div class="header">
+			<h1>Products</h1>
+		</div>
+		
+<?php require_once '../require/end-tags.php'; ?>
