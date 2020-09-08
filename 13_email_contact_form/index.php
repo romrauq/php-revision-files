c83a37f00d3a994459266f518f87458f71699878
John Quarmor <johnequarmor@gmail.com>
2020-09-08 11:21:25 +0000

    created php file containing email processing scripts. Required the created php file in the index page

--- 13_email_contact_form/index.php
+++ 13_email_contact_form/index.php
@@ -1,59 +1,8 @@
 <?php 
 
-	// Message variables:
-	$msg = '';
-	$msgClass = '';
+	// Adding php email processing file as a required file:
+	require './resources/php/email_process.php';	
 
-	// Actions to run after submit button is clicked:
-	if (filter_has_var(INPUT_POST, 'submit')) {
-			
-		// Assign input data into variables:
-		$name = htmlspecialchars($_POST['name']);
-		$email = htmlspecialchars($_POST['email']);
-		$message = htmlspecialchars($_POST['message']);
-
-		# Check required fields/validation:
-		if (!empty($name) && !empty($email) && !empty($message)) {
-		# Condition to run is input fields are not empty:
-				// Check email validity:
-				if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
-					# Email validation failed:
-					$msg = 'Please enter a valid email address';
-					$msgClass = 'warning-class';
-				}else{
-					# Email validation passed actions:
-					// Variable to email details:
-					$recipientEmail = 'abraxismediasupport@gmail.com';
-					$subject = "Contact Request from $name";
-					$emailBody = "<h2>Contact Request</h2>
-							<h4>Name</h4><p>$name</p>
-							<h4>Email</h4><p>$email</p>
-							<h4>Message</h4><p>$message</p>
-					";
-
-					// Email headers:
-					$headers = "MIME-Version: 1.0" . "\r\n";
-					$headers .= "Content-Type: text/html; charset=UTF-8" . "\r\n";
-
-					// Additional headers:
-					$headers .= "From: $name<$email>" . "\r\n";
-
-					if(mail($recipientEmail, $subject, $emailBody, $headers)){
-						// Email sent:
-						$msg = 'Your email has been sent';
-						$msgClass = 'success-class';
-					}else{
-						// Email failed:
-						$msg = 'Your email was not sent';
-						$msgClass = 'warning-class';
-					}
-				}
-		}else{
-			// Actions when all input fields are not filled: 
-			$msg = 'Please fill in all fields';
-			$msgClass = 'warning-class';
-		}
-	}
 ?>
 
 <!DOCTYPE html>
