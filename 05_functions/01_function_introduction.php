9e64bb51ead12dd45171a6507d4d8ca246bff3f4
John Quarmor <johnequarmor@gmail.com>
2020-09-08 12:47:52 +0000

    Minor comment edit

--- 05_functions/01_function_introduction.php
+++ 05_functions/01_function_introduction.php
@@ -2,14 +2,12 @@
 	# A function is a block of code that can be repeated called/executed.
 	# User defined functions are functions that are created by the user/developer.
 	
-	/*
-	Naming conventions for function names:
-	Camel case: myFunction()
-	Lower case: my_function()
-	Pascal case: MyFunction() (Usually used to name classes in OOP)
-	*/
-
-	// Defining/Creating a function:
+	# Naming conventions for function names:
+		// Camel case: myFunction()
+		// Lower case: my_function()
+		// Pascal case: MyFunction() (Usually used to name classes in OOP)
+		
+	// Defining/creating a function:
 	function simpleFunction(){
 	//  This function echoes a specified text whenenever the function is executed/called.
 		echo "Hello World!";
