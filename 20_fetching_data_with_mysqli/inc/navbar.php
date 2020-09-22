74ad92a8d018bdc8e1e1058164928ff58c821bde
John Quarmor <johnequarmor@gmail.com>
2020-09-22 13:24:34 +0000

    Added navbar to 'inc' folder.

---
+++ 20_fetching_data_with_mysqli/inc/navbar.php
@@ -0,0 +1,9 @@
+<div class="navbar">
+<div class="logo">
+	<span>BLOGSPACE</span>
+</div>
+		<div class="navbar-links">
+		<a class="navbar-links" href="<?php echo ROOT_URL ?>">Home</a>
+		<a class="navbar-links" href="<?php echo ROOT_URL ?>addpost.php">Add Post</a>
+	</div>
+</div>
