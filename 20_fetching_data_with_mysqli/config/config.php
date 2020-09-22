7537ee8569fc17a4161054414aac971ec45b84c3
John Quarmor <johnequarmor@gmail.com>
2020-09-22 13:30:40 +0000

    Modified ROOT_URL const variable value.

--- 20_fetching_data_with_mysqli/config/config.php
+++ 20_fetching_data_with_mysqli/config/config.php
@@ -1,7 +1,7 @@
 <?php 
 
 	// Creating const variables to hold database argument values:
-	define('ROOT_URL', 'http://localhost/01_PHP_Revision_Folders/19_fetching_data_with_mysqli/index.php');
+	define('ROOT_URL', 'http://localhost/01_PHP_Revision_Folders/20_fetching_data_with_mysqli/');
 	define('DB_HOST', 'localhost');
 	define('DB_USER', 'root');
 	define('DB_PASSWORD', '');
