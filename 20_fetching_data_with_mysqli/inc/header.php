924b12f2bf797fb0a7ef11266dc5e8c902c9e93d
John Quarmor <johnequarmor@gmail.com>
2020-09-22 13:28:58 +0000

    Added bootstrap to to folder &  link in the <head>

--- 20_fetching_data_with_mysqli/inc/header.php
+++ 20_fetching_data_with_mysqli/inc/header.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="./resources/css/styles.css">
+	<link rel="stylesheet" href="./resources/css/bootstrap.min.css">
 	<title>PHP Blog</title>
 </head>
 <body>
